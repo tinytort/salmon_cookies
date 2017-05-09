18f43fb28c1c345ba5eb634339baebc3661699a6
Haley Tortorella <hdtort@gmail.com>
2017-05-08 17:19:36 -0700

    created complete object 1

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 'use strict';
 
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
+
 var location1 = {
     location: 'PDX Airport',
+    id: 'PDX-Airport',
     minCust: 23,
     maxCust: 65,
     avgCookies: 6.3,
     cookiesPerHr: [],
     custPerHour: function () {
         var randCust = Math.floor(Math.random() * this.maxCust + this.minCust);
-        this.cookiesPerHr.push(randCust * this.avgCookies);
+        return randCust;
+    },
+    calculateCookiesPerHr: function () {
+        for (var i = 0; i < hours.length; i++) {
+            this.cookiesPerHr.push(Math.floor(this.custPerHour() * this.avgCookies));
+        }
+    },
+    cookiePopulator: function () {
+
+        this.calculateCookiesPerHr();
+        var firstLocationCookies = document.getElementById(this.id);
+        firstLocationCookies.innerText = this.location;
+
+        for (var i = 0; i < this.cookiesPerHr.length; i++) {
+            var thisLi = document.createElement('li');
+            thisLi.innerText = hours[i] + ' : ' + this.cookiesPerHr[i] + ' cookies.';
+            firstLocationCookies.appendChild(thisLi);
+            console.log(firstLocationCookies)
+        }
     }
 };
 var location2 = {
@@ -16,7 +37,7 @@ var location2 = {
     minCust: 3,
     maxCust: 24,
     avgCookies: 1.2,
-    custPerHour: function() {
+    custPerHour: function () {
         var randCust = Math.floor(Math.random() * this.maxCust + this.minCust);
         this.cookiesPerHr.push(randCust * this.avgCookies);
     }
@@ -28,18 +49,18 @@ var location3 = {
     minCust: 11,
     maxCust: 38,
     avgCookies: 3.7,
-    custPerHour: function() {
+    custPerHour: function () {
         var randCust = Math.floor(Math.random() * this.maxCust + this.minCust);
         this.cookiesPerHr.push(randCust * this.avgCookies);
     }
 }
 
 var location4 = {
-    location: 'St. John\'s',
+    location: 'St.Johns',
     minCust: 20,
     maxCust: 38,
     avgCookies: 2.3,
-    custPerHour: function() {
+    custPerHour: function () {
         var randCust = Math.floor(Math.random() * this.maxCust + this.minCust);
         this.cookiesPerHr.push(randCust * this.avgCookies);
     }
@@ -50,9 +71,13 @@ var location5 = {
     minCust: 2,
     maxCust: 16,
     avgCookies: 4.6,
-    custPerHour: function() {
+    custPerHour: function () {
         var randCust = Math.floor(Math.random() * this.maxCust + this.minCust);
         this.cookiesPerHr.push(randCust * this.avgCookies);
     }
 }
+
+location1.cookiePopulator();
+
+
 
