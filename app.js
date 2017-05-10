2bbf7bc3d58c2eb24ce34bcd88d2d919f14ad3ea
Haley Tortorella <hdtort@gmail.com>
2017-05-10 15:52:59 -0700

    event will fire when new store is added

--- app.js
+++ app.js
@@ -12,9 +12,8 @@ for (var i = 0; i < hours.length; i++) {
     tableCell.innerText = hours[i]
     tableHeader.appendChild(tableCell)
 }
-function Location(name, id, minCust, maxCust, avgCookies) {
+function Location(name, minCust, maxCust, avgCookies) {
     this.name = name;
-    this.id = id;
     this.minCust = minCust;
     this.maxCust = maxCust;
     this.avgCookies = avgCookies;
@@ -32,8 +31,6 @@ Location.prototype.render = function () {
         else {
             tableCell.innerText = this.cookiesPerHr[i]
             tableRow.appendChild(tableCell)
-            console.log(1)
-
         }
     }
 }
@@ -46,6 +43,7 @@ Location.prototype.custPerHrFn = function () {
 Location.prototype.calculateCookiesPerHr = function () {
     for (var i = 0; i < hours.length; i++) {
         this.cookiesPerHr.push(Math.floor(this.custPerHrFn() * this.avgCookies));
+        console.log(this.avgCookies)
     }
 }
 
@@ -56,23 +54,44 @@ function createCell(id, cellType, content, row) {
     row.appendChild(cell);
 }
 
-var locationOne = new Location('PDX Airport', 'PDX-Airport', 23, 65, 6.3);
+var locationOne = new Location('PDX Airport', 23, 65, 6.3);
 locationOne.calculateCookiesPerHr()
 locationOne.render()
 
-var locationTwo = new Location('Pioneer Square', 'Pioneer-Square', 3, 24, 1.2);
+var locationTwo = new Location('Pioneer Square', 3, 24, 1.2);
 locationTwo.calculateCookiesPerHr()
 locationTwo.render()
 
-var locationThree = new Location('Powell\'s', 'Powells', 11, 38, 3.7);
+var locationThree = new Location('Powell\'s', 11, 38, 3.7);
 locationThree.calculateCookiesPerHr()
 locationThree.render()
 
-var locationFour = new Location('St. John\'s', 'St.Johns', 20, 38, 2.3);
+var locationFour = new Location('St. John\'s', 20, 38, 2.3);
 locationFour.calculateCookiesPerHr()
 locationFour.render()
 
-var locationFive = new Location('Waterfront', 'Waterfront', 2, 16, 4.6);
+var locationFive = new Location('Waterfront', 2, 16, 4.6);
 locationFive.calculateCookiesPerHr()
 locationFive.render()
+
+//Adding new locations from the form 
+
+var newLocation = document.getElementById('addLocation');
+newLocation.addEventListener('submit', addNewLocation);
+
+function addNewLocation() {
+    event.preventDefault();
+    var form = event.target;
+    var name = form.storeName.value;
+    var minimumCust = parseInt(form.minimumCust.value);
+    var maximumCust = parseInt(form.maximumCust.value);
+    var averageCookies = parseInt(form.averageCookies.value);
+    console.log(form, name, minimumCust, maximumCust, averageCookies);
+    var newLocation = new Location(name, minimumCust, maximumCust, averageCookies);
+    console.log(newLocation);
+
+    newLocation.calculateCookiesPerHr();
+    console.log(newLocation.calculateCookiesPerHr());
+    newLocation.render();
+}
 
